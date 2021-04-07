aa8626ab3e0f3d6b124537a5ec0e0872d68d6811
Matthew Brazill <Matthew@Brazill.de>
2021-04-07 22:59:12 +0100

    Expanded on views and contact model. Also added avatar images.

--- app/src/main/java/de/brazill/occursum/activities/CreateContactActivity.kt
+++ app/src/main/java/de/brazill/occursum/activities/CreateContactActivity.kt
@@ -3,18 +3,38 @@ package de.brazill.occursum.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import de.brazill.occursum.R
+import de.brazill.occursum.main.MainApp
+import de.brazill.occursum.models.ContactModel
 import kotlinx.android.synthetic.main.activity_create_contact.*
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 
-class CreateContactActivity : AppCompatActivity(), AnkoLogger {
+class CreateContactActivity: AppCompatActivity(), AnkoLogger {
+
+    var contact = ContactModel()
+    lateinit var app: MainApp
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        info("Create Contact Started...")
+
         setContentView(R.layout.activity_create_contact)
+        app = application as MainApp
 
-        add_contact_button.setOnClickListener() {
-            toast("Contact Added")
+        //Create a event handler to handle the 'Create Contact' button
+        create_contact_button.setOnClickListener() {
+            contact.firstName = first_name.text.toString()
+            contact.lastName = last_name.text.toString()
+            contact.email = email.text.toString()
+            contact.phone = phone.text.toString()
+
+            if (contact.firstName.isNotBlank() && contact.lastName.isNotBlank()) {
+                app.contacts.add(contact.copy())
+                info("Contact '$contact' added.")
+            } else {
+                toast("Please complete all the fields.")
+            }
         }
-
     }
 }
--- app/src/main/java/de/brazill/occursum/models/ContactModel.kt
+++ app/src/main/java/de/brazill/occursum/models/ContactModel.kt
@@ -1,4 +1,11 @@
 package de.brazill.occursum.models
 
-data class ContactModel(var title: String = "") {
-}
+data class ContactModel(
+        var firstName: String = "",
+        var lastName: String = "",
+        var email: String = "",
+        var phone: String = "",
+        var likes: List<String> = listOf(""),
+        var dislikes: List<String> = listOf(""),
+        var img: Int = 0
+)
