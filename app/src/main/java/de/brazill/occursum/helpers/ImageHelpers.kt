7b1496925961d14392972ea637e75ec62bd57ceb
Matthew Brazill <Matthew@Brazill.de>
2021-05-02 19:33:53 +0100

    fixed image permission issue and added center-cropping fit to the images.

--- app/src/main/java/de/brazill/occursum/helpers/ImageHelpers.kt
+++ app/src/main/java/de/brazill/occursum/helpers/ImageHelpers.kt
@@ -1,17 +1,12 @@
 package de.brazill.occursum.helpers
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.ImageDecoder
 import android.net.Uri
-import android.provider.MediaStore
 import android.widget.ImageButton
 import android.widget.ImageView
 import de.brazill.occursum.R
-import java.io.File
 import java.lang.Exception
 
 fun pickImage(parent: Activity, id: Int) {
@@ -26,18 +21,33 @@ fun pickImage(parent: Activity, id: Int) {
 }
 
 fun ImageButton.setImageSafe(path: String) {
-    try {
-        this.setImageBitmap(BitmapFactory.decodeFileDescriptor(context.contentResolver.openFileDescriptor(Uri.parse(path), "r")?.fileDescriptor))
-    } catch (err: Exception) {
+    try /* if (path.startsWith("content://")) */ {
+        this.setImageBitmap(
+            BitmapFactory.decodeFileDescriptor(
+                context.contentResolver.openFileDescriptor(
+                    Uri.parse(path),
+                    "r"
+                )?.fileDescriptor
+            )
+        )
+    } catch (err: Exception) /* else */ {
         this.setImageResource(R.drawable.ic_default_avatar)
     }
     return
 }
 
 fun ImageView.setImageSafe(path: String) {
-    try {
-        this.setImageBitmap(BitmapFactory.decodeFileDescriptor(context.contentResolver.openFileDescriptor(Uri.parse(path), "r")?.fileDescriptor))
-    } catch (err: Exception) {
+    try /* if (path.startsWith("content://")) */ {
+        context.contentResolver.takePersistableUriPermission(Uri.parse(path), Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        this.setImageBitmap(
+            BitmapFactory.decodeFileDescriptor(
+                context.contentResolver.openFileDescriptor(
+                    Uri.parse(path),
+                    "r"
+                )?.fileDescriptor
+            )
+        )
+    } catch (err: Exception) /* else */ {
         this.setImageResource(R.drawable.ic_default_avatar)
     }
     return
